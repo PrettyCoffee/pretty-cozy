04f43d2c41afb4a824c78c6e744b8a45fb1e3a6b
PrettyCoffee <jonathan-holz@outlook.com>
2025-03-23 00:32:47 +0100

    feat(eslint-config): Add vitest config

---
+++ packages/eslint-config/configs/vitest.js
@@ -0,0 +1,31 @@
+import vitestPlugin from "@vitest/eslint-plugin"
+import globals from "globals"
+import ts from "typescript-eslint"
+
+export default ts.config({
+  name: "@ptvgroup/vitest",
+  files: ["**/*.test.*"],
+  plugins: {
+    vitest: vitestPlugin,
+  },
+  languageOptions: {
+    globals: globals.vitest,
+  },
+  rules: {
+    ...vitestPlugin.configs.recommended.rules,
+    "vitest/no-disabled-tests": "off",
+
+    "vitest/no-alias-methods": "error",
+    "vitest/no-conditional-expect": "error",
+    "vitest/no-done-callback": "error",
+    "vitest/no-focused-tests": "error",
+    "vitest/no-interpolation-in-snapshots": "error",
+    "vitest/no-mocks-import": "error",
+    "vitest/no-standalone-expect": "error",
+    "vitest/no-test-prefixes": "error",
+    "vitest/valid-expect-in-promise": "error",
+    "vitest/prefer-to-be": "error",
+    "vitest/prefer-to-contain": "error",
+    "vitest/prefer-to-have-length": "error",
+  },
+})
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -4,5 +4,6 @@ import preact from "./configs/preact.js"
 import prettier from "./configs/prettier.js"
 import react from "./configs/react.js"
 import tailwind from "./configs/tailwind.js"
+import vitest from "./configs/vitest.js"
 
-export default { baseJs, baseTs, react, preact, prettier, tailwind }
+export default { baseJs, baseTs, react, preact, prettier, tailwind, vitest }
