e476760a581d2f5f874441263f156126e5bbbb15
PrettyCoffee <jonathan-holz@outlook.com>
2025-03-23 00:33:53 +0100

    refactor(eslint-config): Assign config name to prettier conflicts

--- packages/eslint-config/configs/prettier.js
+++ packages/eslint-config/configs/prettier.js
@@ -2,25 +2,30 @@ import prettierConfig from "eslint-config-prettier"
 import prettierPlugin from "eslint-plugin-prettier"
 import ts from "typescript-eslint"
 
-export default ts.config({
-  name: "@pretty-cozy/prettier",
-  extends: [prettierConfig],
-  plugins: {
-    prettier: prettierPlugin,
+export default ts.config(
+  {
+    ...prettierConfig,
+    name: "@pretty-cozy/prettier/disable-conflicts",
   },
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        tabWidth: 2,
-        singleQuote: false,
-        trailingComma: "es5",
-        semi: false,
-        bracketSpacing: true,
-        arrowParens: "avoid",
-        endOfLine: "auto",
-        jsxSingleQuote: false,
-      },
-    ],
-  },
-})
+  {
+    name: "@pretty-cozy/prettier",
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          tabWidth: 2,
+          singleQuote: false,
+          trailingComma: "es5",
+          semi: false,
+          bracketSpacing: true,
+          arrowParens: "avoid",
+          endOfLine: "auto",
+          jsxSingleQuote: false,
+        },
+      ],
+    },
+  }
+)
