e9a25536cef18058775dd33025b920718c64f4f3
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2026-02-03 20:57:41 +0100

    feat(release-tool)!: Remove npm login and publish commands

--- packages/release-tools/src/npm.ts
+++ packages/release-tools/src/npm.ts
@@ -13,26 +13,6 @@ const run = async (script: string) => {
   }
 }
 
-/** Get the current user's npm username.
- * @returns A promise that resolves to the username
- */
-const whoAmI = async () => {
-  try {
-    return await exec(`npm whoami`)
-  } catch {
-    return null
-  }
-}
-
-interface LoginArgs {
-  scope?: string
-}
-/** Log in to npm with the specified scope.
- * @param args.scope The npm scope to use.
- */
-const login = async (args: LoginArgs) =>
-  exec(`npm login ${stringifyArgs(args as Record<string, unknown>)}`)
-
 interface InstallArgs {
   force?: boolean
   dryRun?: boolean
@@ -46,26 +26,7 @@ const install = async (args: InstallArgs) => {
   await exec(`npm i ${stringifyArgs(args as Record<string, unknown>)}`)
 }
 
-interface PublishArgs {
-  dryRun?: boolean
-  workspace?: string | string[]
-  workspaces?: boolean
-  access?: "restricted" | "public"
-}
-/** Publish npm packages.
- * @param args.dryRun If set to true, the changes are not applied.
- * @param args.workspace The specific workspace(s) to publish.
- * @param args.workspaces If set to true, all workspaces are published.
- * @param args.access Whether the access to the module should be public or restricted.
- * @returns A promise that resolves when all packages are published.
- */
-const publish = (args: PublishArgs) =>
-  exec(`npm publish ${stringifyArgs(args as Record<string, unknown>)}`)
-
 export const npm = {
   run,
-  whoAmI,
-  login,
   install,
-  publish,
 }
